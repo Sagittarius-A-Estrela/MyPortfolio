0f78e0e0839ea38dee9b33f147d8100e97b4b3f4
Mateus Oliveira Duarte <dev.01101101.01100100@gmail.com>
2022-12-20 00:29:36 -0300

    teste
    add jQuery

--- script/script.js
+++ script/script.js
@@ -1,20 +1,17 @@
-function callClose(){
-    document.getElementById('content').style.display = 'none';
-    setTimeout(callMaximize, 5000);
-}
-
-function callMaximize(){
-    document.location.reload(true);
-}
-
-function callMinimize(){
-    document.getElementById('content').style.position = 'absolute';
-    document.getElementById('content').style.width = '20%';
-    document.getElementById('content').style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
-    document.getElementById('content').style.bottom = '0';
-    document.getElementById('content').style.left = '5px';
-    document.getElementById('content').style.borderRadius = '5px 5px 0 0';
-    document.getElementById('content').style.border = '1px solid rgba( 255, 255, 255, 0.2)';
-    document.getElementById('content').style.transition = 'bottom 5s';
-    document.getElementById('main-content').style.display = 'none';
-}
+$(document).ready(function(){
+    $(".control-close").click(function(){
+        $(".window").addClass("window--destroyed");
+        $(".window").removeClass("window--maximized window--minimized");
+        setTimeout(location.reload.bind(location), 5000);
+    }),
+      
+    $(".control-maximized").click(function(){
+        $(".window").removeClass("window--minimized");
+        $(".window").toggleClass("window--maximized");
+    }),
+  
+    $(".control-minimized").click(function(){
+        $(".window").removeClass("window--maximized");
+        $(".window").toggleClass("window--minimized");
+    });
+});
